b4ab9420506428e7172cb97bbe1381255c9f6c7e
Arthur Brongniart <arthur.brongniart.esgi@gmail.com>
2022-01-13 15:12:50 +0100

    add resource databricks-project

--- main.tf
+++ main.tf
@@ -2,3 +2,10 @@ resource "azurerm_resource_group" "firstdataproject" {
  name		= "firstdataproject"
  location	= "West Europe"
  }
+
+resource "azurerm_databricks_workspace" "databricks-firstdataproject" {
+  name                = "databricks-firstdataproject"
+  resource_group_name = azurerm_resource_group.firstdataproject.name
+  location            = azurerm_resource_group.firstdataproject.location
+  sku                 = "trial"
+}
