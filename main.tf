15c8c0386775a8712cfda55f610a6c21c9ec62ef
Arthur Brongniart <arthur.brongniart.esgi@gmail.com>
2022-01-13 20:26:49 +0100

    delete some part

--- main.tf
+++ main.tf
@@ -10,39 +10,3 @@ resource "azurerm_databricks_workspace" "databricks-firstdataproject" {
   sku                 = "trial"
 }
 
-resource "azurerm_storage_account" "blobaccount" {
-  name                     = "esgiblobuwucute"
-  resource_group_name = azurerm_resource_group.firstdataproject.name
-  location            = azurerm_resource_group.firstdataproject.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-  account_kind             = "StorageV2"
-}
-
-resource "azurerm_storage_container" "marketing" {
-  name                  = "marketing"
-  storage_account_name  = azurerm_storage_account.esgiblobuwucute.name
-  container_access_type = "private"
-}
-
-resource "databricks_secret_scope" "terraform" {
-  name                     = "application"
-  initial_manage_principal = "users"
-}
-
-resource "databricks_secret" "storage_key" {
-  key          = "blob_storage_key"
-  string_value = azurerm_storage_account.esgiblobuwucute.primary_access_key
-  scope        = databricks_secret_scope.terraform.name
-}
-
-resource "databricks_mount" "marketing" {
-  name = "marketing"
-  wasb {
-    container_name       = azurerm_storage_container.marketing.name
-    storage_account_name = azurerm_storage_account.esgiblobuwucute.name
-    auth_type            = "ACCESS_KEY"
-    token_secret_scope   = databricks_secret_scope.terraform.name
-    token_secret_key     = databricks_secret.storage_key.key
-  }
-}
